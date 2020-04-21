703aed49e61a0227e9b79b22cf6118b16b974565
valerybuko <valpal314@gmail.com>
2020-04-21 14:34:51 +0300

    add selectors

--- src/store.js
+++ src/store.js
@@ -3,12 +3,11 @@ import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import thunk from 'redux-thunk';
-import { composeWithDevTools } from "redux-devtools-extension";
-import { todos, isLoading } from './todos/reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { todos } from './todos/reducers';
 
 const reducers = {
     todos,
-    isLoading,
 };
 
 const persistConfig = {
